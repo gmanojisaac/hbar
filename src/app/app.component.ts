38e26cee5c6885bdd3ac4d25fa2442669ad32a54
merlinpratheesh <merlinpratheesh@gmail.com>
2021-02-01 13:01:41 +0530

    Hedera started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Client, PrivateKey, AccountCreateTransaction, AccountBalanceQuery, Hbar }from '@hashgraph/sdk';
+import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HBAR';
+  myAccountId = environment.accountId;
+  myPrivateKey = environment.privateKey;
+  constructor(){
+    const client = Client.forTestnet();    
+    client.setOperator(this.myAccountId, this.myPrivateKey);
+
+    const newAccountTransactionId = new AccountCreateTransaction()
+    .setKey(environment.publicKey)
+    .setInitialBalance(Hbar.fromTinybars(1000))
+    .execute(client);
+    console.log(newAccountTransactionId);
+  }
 }
