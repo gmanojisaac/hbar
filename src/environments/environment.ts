37d66b0560cab96d7f96bd08a29490a71675a91a
gmanojisaac <gmanoj.isaac@gmail.com>
2021-02-02 12:34:49 +0530

    Done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
   accountId: "0.0.293822",
   publicKey: "302a300506032b657003210055c0256d07df71f27b5a9edf01bed7f06d9ac82f25c751ed8384d3eeacc0f69e",
   privateKey: "302e020100300506032b657004220420a9091d648d6ac4a01b0b66e3f63f7bde877c9a9aa16bbde0b076dc01cef432ab"
+
 };
 
 /*
